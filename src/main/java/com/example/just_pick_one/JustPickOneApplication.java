1090e3a55e5efabb1cb723b3229e38deaa4dd6fe
Angie <angiepolitte@gmail.com>
2025-04-29 12:11:02 -0500

    Init commit

---
+++ src/main/java/com/example/just_pick_one/JustPickOneApplication.java
@@ -0,0 +1,13 @@
+package com.example.just_pick_one;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustPickOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JustPickOneApplication.class, args);
+	}
+
+}
